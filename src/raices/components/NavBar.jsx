2a21111252780259b0de982110719069a38d52c8
quique <henrix427@gmail.com>
2023-03-30 02:16:46 +0200

    add styles to NavBar

--- src/raices/components/NavBar.jsx
+++ src/raices/components/NavBar.jsx
@@ -1,17 +1,92 @@
-import { MenuOutlined } from '@mui/icons-material'
-import { AppBar, IconButton, Toolbar } from '@mui/material'
+import { ContactPhone, ContactPhoneOutlined,  LoginOutlined, Search } from '@mui/icons-material'
+import { AppBar, Button, Divider, IconButton, InputBase, Toolbar, Typography } from '@mui/material'
+import { Box, Stack } from '@mui/system'
+
+const inputCSS = {
+    width: '100%',
+    fontSize: '14px',
+    border: '1px solid red',
+    borderColor: 'red'
+}
 
 export const NavBar = () => {
-  return (
-    <AppBar
-        position="fixed"
-    >
-        <Toolbar>
-            <IconButton>
-                <MenuOutlined/>
-            </IconButton>
-        </Toolbar>
+    return (
+        <AppBar
+            position="fixed"
+            sx={{ height: 126, width: '100%'}}
+        >
+            <Toolbar 
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-end',
+                    alignContent: 'space-around'
+                }}
+            >
+                <Stack direction="row" 
+                    divider={<Divider orientation="vertical" flexItem />} 
+                    spacing={1}
+                    sx={{
+                        backgroundColor: 'secondary.main', 
+                        color: 'inherit',
+                        height: `calc(100% - 22px)`,
+                        marginTop: '-22px',
+                     }}
+                >
+                    <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
+                        <Search />
+                        <InputBase
+                            sx={{ ml: 1, }}
+                            placeholder="Cinco marzada"
+                        />
+                    </Box>
+                    <Button variant="outlined" 
+                        startIcon={<ContactPhoneOutlined />}
+                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                    >
+                        Contacto
+                    </Button>
+                    <Button variant="outlined" 
+                        startIcon={<LoginOutlined />}
+                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                    >
+                        Acceso usuarios
+                    </Button>
+                </Stack>
 
-    </AppBar>
-  )
+
+                <Stack direction="row" 
+                    divider={<Divider orientation="vertical" flexItem />} 
+                    spacing={1}
+                    sx={{
+                        backgroundColor: 'secondary.main', 
+                        color: 'inherit',
+                        height: `calc(100% - 22px)`,
+                        marginTop: '-22px',
+                     }}
+                >
+                    <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
+                        <Search />
+                        <InputBase
+                            sx={{ ml: 1, }}
+                            placeholder="Cinco marzada"
+                        />
+                    </Box>
+                    <Button variant="outlined" 
+                        startIcon={<ContactPhoneOutlined />}
+                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                    >
+                        Contacto
+                    </Button>
+                    <Button variant="outlined" 
+                        startIcon={<LoginOutlined />}
+                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                    >
+                        Acceso usuarios
+                    </Button>
+                </Stack>
+            </Toolbar>
+
+        </AppBar>
+    )
 }
