58b9386e3fddd17addaf8f02729e52e838a3fd8d
Enriquee <henrix427@gmail.com>
2023-03-31 17:25:04 +0200

    include logo into the Toolbar and wrap Stacks in a Box

--- src/raices/components/NavBar.jsx
+++ src/raices/components/NavBar.jsx
@@ -8,14 +8,14 @@ const StyledStackYellow = styled(Stack)(({ theme }) => ({
     color: 'inherit',
     height: `calc(33%)`,
     marginTop: 0,
-    alignSelf: 'flex-start'
+    alignSelf: 'flex-end',
 }));
 const StyledStackRed = styled(StyledStackYellow)(({ theme }) => ({
     backgroundColor: theme.palette.error.main,
     color: theme.palette.primary.main,
     height: `calc(33%)`,
     alignSelf: 'flex-end',
-    flexGrow: 1,
+    // flexGrow: 1,
     justifyContent: 'flex-end'
 }));
 
@@ -23,102 +23,98 @@ export const NavBar = () => {
     return (
         <AppBar
             position="fixed"
-            sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                // alignItems: 'flex-end',
-                justifyContent: 'space-between',
-                height: 126,
-                width: '100%'
-            }}
-        >
-            {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Raices Andinas
-            </Typography> */}
-            
-            {/* <AppBar position="static">
-                <Toolbar>
-                    <Box
-                        component="img"
-                        sx={{
-                            height: 64,
-                        }}
-                        alt="Your logo."
-                        src={Logo}
-                    />
-                    ...
-                </Toolbar>
-            </AppBar> */}
-            <Box
-                component="img"
-                sx={{ height: 125 }}
-                alt="Logo"
-                src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_e7b8a53bf9ee1f0023a60ce6644dd5f8/flaticon.jpg"
-            />
+            sx={{ height: 126, width: '100%' }}
+        >           
 
             <Toolbar disableGutters
                 sx={{
                     height: '100%',
-                    width: '66%',
-                    pl: 0,
+                    width: '100%',
                     display: 'flex',
-                    flexWrap: 'wrap',
-                    // flexDirection: 'column',
-                    justifyContent: 'flex-end',
-                    // alignItems: 'flex-end',
-                    alignContents: 'space-between',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
                 }}
             >
+                 <Box
+                    component="img"
+                    sx={{ height: 125 }}
+                    alt="Logo"
+                    src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_e7b8a53bf9ee1f0023a60ce6644dd5f8/flaticon.jpg"
+                />
 
-                <StyledStackYellow direction="row"
-                    divider={<Divider orientation="vertical" flexItem />}
-                    spacing={1}
+                <Box component="div" 
+                    sx={{
+                        // / flexGrow: 1,
+                        height: '100%',
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                    }}
                 >
-                    <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
-                        <Search />
-                        <InputBase
-                            sx={{ ml: 1, }}
-                            placeholder="algo"
-                        />
-                    </Box>
-                    <Button variant="text"
-                        startIcon={<ContactPhoneOutlined />}
-                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
-                    >
-                        Contacto
-                    </Button>
-                    <Button variant="text"
-                        startIcon={<LoginOutlined />}
-                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
-                    >
-                        Acceso usuarios
-                    </Button>
-                </StyledStackYellow>
 
-                <StyledStackRed direction="row"
-                    divider={<Divider orientation="vertical" flexItem />}
-                    spacing={1}
-                >
-                    <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
-                        <Search />
-                        <InputBase
-                            sx={{ ml: 1, }}
-                            placeholder="algo"
-                        />
-                    </Box>
-                    <Button variant="text"
-                        startIcon={<ContactPhoneOutlined />}
-                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                    <StyledStackYellow direction="row"
+                        divider={<Divider orientation="vertical" flexItem />}
+                        spacing={1}
                     >
-                        Contacto
-                    </Button>
-                    <Button variant="text"
-                        startIcon={<LoginOutlined />}
-                        sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                        <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
+                            <Search />
+                            <InputBase
+                                sx={{ ml: 1, }}
+                                placeholder="algo"
+                            />
+                        </Box>
+                        <Button variant="text"
+                            startIcon={<ContactPhoneOutlined />}
+                            sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                        >
+                            Contacto
+                        </Button>
+                        <Button variant="text"
+                            startIcon={<LoginOutlined />}
+                            sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                        >
+                            Acceso usuarios
+                        </Button>
+                    </StyledStackYellow>
+
+                    <StyledStackRed direction="row"
+                        divider={<Divider orientation="vertical" flexItem />}
+                        spacing={1}
                     >
-                        Acceso usuarios
-                    </Button>
-                </StyledStackRed>
+                        <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
+                            <Search />
+                            <InputBase
+                                sx={{ ml: 1, }}
+                                placeholder="algo"
+                            />
+                        </Box>
+                        <Button variant="text"
+                            startIcon={<ContactPhoneOutlined />}
+                            sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                        >
+                            Contacto
+                        </Button>
+                        <Button variant="text"
+                            startIcon={<ContactPhoneOutlined />}
+                            sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                        >
+                            Contacto
+                        </Button>
+                        <Button variant="text"
+                            startIcon={<ContactPhoneOutlined />}
+                            sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                        >
+                            Contacto
+                        </Button>
+                        <Button variant="text"
+                            startIcon={<LoginOutlined />}
+                            sx={{ backgroundColor: 'transparent', color: 'inherit' }}
+                        >
+                            Acceso usuarios
+                        </Button>
+                    </StyledStackRed>
+
+                </Box>
 
 
             </Toolbar>
