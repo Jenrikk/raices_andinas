89aa31a2cd4d95a570788ab38f985bccacdaa39d
quique <henrix427@gmail.com>
2023-04-12 21:53:32 +0200

    Add floating button to go to the top

--- src/raices/pages/RaicesPage.jsx
+++ src/raices/pages/RaicesPage.jsx
@@ -1,6 +1,7 @@
 import { RaicesLayout } from "../layout/RaicesLayout"
-import { Typography } from '@mui/material'
+import { IconButton, Typography } from '@mui/material'
 import { LoadingView, MainView } from "../views"
+import { ArrowDropUp, KeyboardDoubleArrowUp, UpcomingOutlined } from "@mui/icons-material"
 
 
 export const RaicesPage = () => {
@@ -12,6 +13,21 @@ export const RaicesPage = () => {
 
 
       <MainView />
+
+      <IconButton 
+        size="large" 
+        sx={{
+            color: 'white', 
+            backgroundColor: 'error.main',
+            ':hover': {backgroundColor: 'error.main', opacity: 0.8},
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+        }}>
+
+          <KeyboardDoubleArrowUp sx={{ fontSize: 30 }} />
+
+      </IconButton>
       
 
     </RaicesLayout>
